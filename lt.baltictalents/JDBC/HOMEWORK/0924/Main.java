473ea46cb3bc9c9eb227f0e00a9cc6a8cb0a0095
apzivile <39947097+apzivile@users.noreply.github.com>
2018-09-26 17:30:19 +0300

    Create Main.java

---
+++ lt.baltictalents/JDBC/HOMEWORK/0924/Main.java
@@ -0,0 +1,12 @@
+package lt.baltictalents.hw0924;
+
+import java.io.IOException;
+
+import static lt.baltictalents.hw0924.Query.createNewTable;
+
+public class Main   {
+    public static void main(String[] args) throws IOException {
+        createNewTable();
+
+    }
+}
